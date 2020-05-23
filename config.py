14bf56abc3b03300925e4370d8f093b287f1f24e
kp <work4kirti@gmail.com>
2020-05-23 15:07:51 -0400

    Typo correction in Security Group comments section

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ MASTERINSTANCETYPE='m5.xlarge'
 SLAVEINSTANCETYPE='m5.xlarge'
 INSTANCECOUNT=3
 
-# Security Groups (get from AWS accoutn and fill this out 
+# Security Groups (get from AWS account and fill this out 
 # these lines in lanuch_cluster.py to use with notebook)
 # If not, these 3 lines below can be commented out for job execution
 EC2SUBNETID='subnet-xyz0123'
